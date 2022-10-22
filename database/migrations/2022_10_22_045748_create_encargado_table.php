efcfb932d25e4ba79824990001024d9f699eab21
3dwintny <tecnotony8@gmail.com>
2022-10-21 23:12:45 -0600

    Second  migrtion

---
+++ database/migrations/2022_10_22_045748_create_encargado_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('encargados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre1',30);
+            $table->string('nombre2',30);
+            $table->string('nombre3',30);
+            $table->string('apellido1',30);
+            $table->string('apellido2',30);
+            $table->string('apellido_casada',30);
+            $table->string('direccion',150);
+            $table->string('celular',10)->unique();
+            $table->string('telefono_casa',10)->unique();
+            $table->string('correo',100)->unique();
+            $table->string('dpi',25)->unique();
+            $table->unsignedInteger('id_parentezco');
+            $table->foreign('id_parentezco')->references('id')->on('parentezco');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('encargados');
+    }
+};
