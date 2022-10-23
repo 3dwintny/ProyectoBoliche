42d73e32d4b46707eaf68e665b08a60ceebf95c2
CCaceres1004 <oscar.caceres2321@gmail.com>
2022-10-22 21:26:26 -0600

    Creación de los Controladores

---
+++ app/Http/Controllers/Deporte_AdoptadoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Deporte_AdoptadoController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Nivel_fadnController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Nivel_fadnController extends Controller
+{
+    //
+}
