706c79750a95bb2eaeca852bace07f7e4df06405
axelhp25 <axelhp25>
2022-10-24 01:23:37 -0600

    Cambio en inicio

---
+++ resources/views/Alumnos/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('alumnos')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+              
+                <form>
+                    <label for="">Nombre</label>
+                    <input type="text" name="" id="">
+                </form>
+
+            
+        </div>
+    </div>
+</div>
+@endsection
