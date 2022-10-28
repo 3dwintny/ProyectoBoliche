f9d29326f9c55707c3f1a126468b9794f1abcae7
CCaceres1004 <oscar.caceres2321@gmail.com>
2022-10-27 21:28:48 -0600

    Mejoras estéticas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrarse</a>
                         @endif
-                        <a href="{{route('alumnos.index')}}" class="text-sm text-gray-700 dark:text-gray-500 underline">Formulario Inscripción</a>
+                        <a href="{{route('alumnos.index')}}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Formulario Inscripción</a>
                     @endauth
                 </div>
             @endif
