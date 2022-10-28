aabe94d830d2a8fcaacf0571dc7592b503fc2bca
CCaceres1004 <oscar.caceres2321@gmail.com>
2022-10-27 23:36:26 -0600

    Procedimientos respectivos para el almacenamiento de nuevos usuarios

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,12 +39,14 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'id_tipo_usuario' => ['required'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'id_tipo_usuario' => $request->id_tipo_usuario,
         ]);
 
         event(new Registered($user));
