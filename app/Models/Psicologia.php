27f257dab572745558c124ea8df61e4f0182a831
CCaceres1004 <oscar.caceres2321@gmail.com>
2022-11-01 23:00:27 -0600

    Modelos de Psicologia y Entrenador

--- app/Models/Psicologia.php
+++ app/Models/Psicologia.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Psicologia extends Model
 {
     use HasFactory;
+    protected $table="psicologia";
+    protected $fillable = ['id','nombre1','nombre2','nombre3','apellido1','apellido2','apellido_casada',
+'colegiado','telefono','correo','direccion','fecha_inicio_labores','created_at','updated_at'];
 }
