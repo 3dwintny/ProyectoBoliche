7c795a4918ee61fdfc7d4239e1aa5c62ca891feb
CCaceres1004 <oscar.caceres2321@gmail.com>
2023-06-14 15:11:03 -0600

    subiendo cambios

--- resources/views/Atletas/pendientes.blade.php
+++ resources/views/Atletas/pendientes.blade.php
@@ -7,7 +7,7 @@
             <!-- Card stats -->
             <div class="row">
                 <div class="col-xl-6 col-lg-6">
-                    <h1 class="text-white">Tareas pendientes</h1>
+                    <h1 class="text-white">Tareas pendientes {{$nombre}}</h1>
                 </div>
             </div>
         </div>
