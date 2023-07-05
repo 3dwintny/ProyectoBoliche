573d5bdb7d6f41df4409946ca53782a4fe35c8b2
CCaceres1004 <oscar.caceres2321@gmail.com>
2023-07-05 12:53:10 -0600

    subiendo cambios

--- resources/views/configuraciones/categoria/control.blade.php
+++ resources/views/configuraciones/categoria/control.blade.php
@@ -1,17 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.headers.cards', ['texto' => 'Categorias'])
-<div class="container">
-  <div class="pb-5 pt-5 pt-md-2 mt--5">
-    @if(session('error'))
-      <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{ session('error') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="header bg-dark pb-3 pt-xl-5 pt-lg-5 pt-md-2 pt-sm-2">
+  <div class="container-fluid">
+      <div class="header-body">
+          <div class="row">
+              <div class="col-xl-6 col-lg-6 col-md-10 col-sm-6">
+                  <h1 class="text-white">Categorías</h1>
+              </div>
+          </div>
       </div>
-    @endif
-    <div class="">
-      <table class="table table-responsive table-hover" style="border-radius: 5px;">
+  </div>
+</div>
+<div class="container-fluid pt-2">
+  <div class="header-body text-center mb-7">
+    <div class="row justify-content-center">
+      @if(session('error'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          {{ session('error') }}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      @endif
+      <div class="col-xl-6 col-lg-6 col-md-10 col-sm-8">
+        <table class="table table-responsive table-hover" style="border-radius: 5px;">
         <thead class="table-dark">
           <tr>
             <th scope="col">No</th>
@@ -42,7 +53,8 @@
             @endforeach
           @endif
         </tbody>
-      </table>
+        </table>
+      </div>
       {{$control->links('vendor.pagination.custom')}}
     </div>
   </div>
--- resources/views/configuraciones/categoria/edit.blade.php
+++ resources/views/configuraciones/categoria/edit.blade.php
@@ -1,51 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-<!--div class="body pb-8 pt-5 pt-lg-1 d-flex align-items-center" style="background-image: url(../argon/img/theme/bol.jpg); background-size: cover; background-position: center top;"!-->
-@include('layouts.headers.cards', ['texto' => 'Categorias'])
-<div class="container-fluid mt--5">
-    <div class="header-body text-center  mb-7">
-        <div class="row justify-content-center">
-            <div class="col-lg-8 col-md-8">
-                <div class="card">
-                    <div class="card-header text-bold ">
-                        <strong>
-                            <h2>Editar Categoría </h2>
-                        </strong>
-                    </div>
-                    @if(session('error'))
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ session('error') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    @endif
-                <form method="post" role="form" enctype="multipart/form-data" action="{{route('categoria.update',encrypt($categoria->id))}}">
-                    @csrf
-                    {{method_field('PUT')}}
-                    <div class="card">
-                        <div class="card-body bg-light">
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="tipo" placeholder="Categoría" name="tipo" value="{{$categoria->tipo}}" required>
-                                <label for="tipo">Categoría</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="rango_edades" placeholder="Rango de Edades" name="rango_edades" value="{{$categoria->rango_edades}}">
-                                <label for="tipo">Rango de Edades</label>
-                            </div>
-                            <div class="container">
-                                <div class="col-md-4 mb-10 center"><button type="submit" class="btn btn-outline-primary">Actualizar</button></div>
-                            </div>
-                </form>
-
-
+<div class="header bg-dark pb-3 pt-xl-5 pt-lg-5 pt-md-2 pt-sm-2">
+    <div class="container-fluid">
+        <div class="header-body">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-10 col-sm-6">
+                    <h1 class="text-white">Categorías</h1>
+                </div>
             </div>
         </div>
     </div>
-</div>
-</div>
+  </div>
+<div class="container-fluid pt-2">
+    <div class="header-body text-center mb-7">
+        <div class="row justify-content-center">
+            @if(session('error'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ session('error') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            <div class="col-xl-6 col-lg-6 col-md-10 col-sm-8">
+                <div class="card">
+                    <div class="card-header text-bold ">
+                        <strong>
+                            <h2>Editar categoría </h2>
+                        </strong>
+                    </div>
+                    <form method="post" role="form" enctype="multipart/form-data" action="{{route('categoria.update',encrypt($categoria->id))}}">
+                        @csrf
+                        {{method_field('PUT')}}
+                        <div class="card">
+                            <div class="card-body bg-light">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="tipo" placeholder="Categoría" name="tipo" value="{{$categoria->tipo}}" required>
+                                    <label for="tipo">Categoría</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="rango_edades" placeholder="Rango de edades" name="rango_edades" value="{{$categoria->rango_edades}}">
+                                    <label for="tipo">Rango de edades</label>
+                                </div>
+                                <div class="container">
+                                    <div class="col-md-4 mb-10 center"><button type="submit" class="btn btn-outline-primary">Actualizar</button></div>
+                                </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
