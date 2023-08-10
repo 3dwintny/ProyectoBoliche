e8a131575c761d6b6bd0d9a7c03b4f801bba4687
CCaceres1004 <oscar.caceres2321@gmail.com>
2023-08-10 02:07:23 -0600

    Subiendo cambios

--- resources/views/Atletas/asistencia.blade.php
+++ resources/views/Atletas/asistencia.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="header bg-dark pb-4 pt-5 pt-md-6 mt--5">
+<div class="header bg-dark pb-3 pt-xl-5 pt-lg-5 pt-md-2 pt-sm-2">
     <div class="container-fluid">
         <div class="header-body">
             <div class="row">
-                <div class="col-xl-6 col-lg-6">
+                <div class="col-xl-6 col-lg-8 col-md-10 col-sm-12">
                     <h1 class="text-white">Asistencia de {{ $mostrarMes }} de {{ $obtenerAnio }}</h1>
                 </div>
             </div>
         </div>
     </div>
-</div>
+  </div>
 
 <div class="pb-4 pt-5 pt-md-3">
     <div class="container">
