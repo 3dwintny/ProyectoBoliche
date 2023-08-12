4cde01821854ca0c7e5a80b074c8d1a5faa7d770
CCaceres1004 <oscar.caceres2321@gmail.com>
2023-08-12 10:26:09 -0600

    Subiendo cambios

--- resources/views/configuraciones/psicologia/show.blade.php
+++ resources/views/configuraciones/psicologia/show.blade.php
@@ -44,33 +44,39 @@
           @php
               $contador = 1;   
           @endphp
-          @foreach ($psicologo as $item)
+          @if(count($psicologo)<=0)
           <tr>
-            <td>{{$contador}}</td>
-            <td>{{$item->nombre1}} {{$item->nombre2}} {{$item->nombre3}} {{$item->apellido1}} {{$item->apellido2}} {{$item->apellido_casada}}</td>
-            <td>{{$item->colegiado}}</td>
-            <td>{{$item->telefono}}</td>
-            <td>{{$item->correo}}</td>
-            <td>{{$item->direccion}}</td>
-            <td>{{\Carbon\Carbon::parse($item->fecha_inicio_labores)->format("d-m-Y")}}</td>
-            <td>
-              <form action="{{route('psicologia.edit',encrypt($item->id))}}" method="GET">
-                @csrf
-                <button class="btn btn-primary" type="submit"><i class="fa fa-fw fa-regular fa-edit"></i></button>
-              </form>
-            </td>
-            <td>
-              <form action="{{route('psicologia.destroy',encrypt($item->id))}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button class="btn btn-danger" type="submit" onclick="return eliminarPsicologia('Eliminar psicológa(o)')"><i class="fa fa-fw fa-regular fa-trash"></i></button>
-              </form>
-            </td>
-            @php
-              $contador++;
-            @endphp
+            <td colspan="7" class="textoCentrado textoNegrita">NO SE ENCONTRARON RESULTADOS</td>
           </tr>
-          @endforeach
+          @else
+            @foreach ($psicologo as $item)
+            <tr>
+              <td>{{$contador}}</td>
+              <td>{{$item->nombre1}} {{$item->nombre2}} {{$item->nombre3}} {{$item->apellido1}} {{$item->apellido2}} {{$item->apellido_casada}}</td>
+              <td>{{$item->colegiado}}</td>
+              <td>{{$item->telefono}}</td>
+              <td>{{$item->correo}}</td>
+              <td>{{$item->direccion}}</td>
+              <td>{{\Carbon\Carbon::parse($item->fecha_inicio_labores)->format("d-m-Y")}}</td>
+              <td>
+                <form action="{{route('psicologia.edit',encrypt($item->id))}}" method="GET">
+                  @csrf
+                  <button class="btn btn-primary" type="submit"><i class="fa fa-fw fa-regular fa-edit"></i></button>
+                </form>
+              </td>
+              <td>
+                <form action="{{route('psicologia.destroy',encrypt($item->id))}}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit" onclick="return eliminarPsicologia('Eliminar psicológa(o)')"><i class="fa fa-fw fa-regular fa-trash"></i></button>
+                </form>
+              </td>
+              @php
+                $contador++;
+              @endphp
+            </tr>
+            @endforeach
+          @endif
         </tbody>
       </table>
     </div>
@@ -90,4 +96,7 @@
 @push('js')
 <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
 <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
+@push('styles')
+  <link rel="stylesheet" href="css/general.css">
 @endpush
--- resources/views/entrenador/index.blade.php
+++ resources/views/entrenador/index.blade.php
@@ -47,39 +47,45 @@
           @php
             $contador = 1;
           @endphp
-          @foreach ($entrenadores as $entrenador)
-          <tr>
-            <td>{{$contador}}</td>
-            <td>{{$entrenador->nombre1}} {{$entrenador->nombre2}} {{$entrenador->nombre3}} {{$entrenador->apellido1}} {{$entrenador->apellido2}} {{$entrenador->apellido_casada}}</td>
-            <td>{{$entrenador->cui}}</td>
-            <td>{{$entrenador->estado_civil}}</td>
-            <td>{{$entrenador->celular}}</td>
-            <td>{{$entrenador->correo}}</td>
-            <td>{{$entrenador->edad}}</td>
-            <td>{{$entrenador->direccion}}</td>
-            <td>{{$entrenador->deporte->nombre}}</td>
-            <td>
-              <form action="{{route('entrenadores.edit',encrypt($entrenador->id))}}" method="GET">
-                <button class="btn btn-primary" type="submit"><i class="fa fa-fw fa-regular fa-edit"></i></button>
-              </form>
-            </td>
-            <td>
-              <form action="{{route('entrenadores.show',encrypt($entrenador->id))}}" action="GET">
-                <button class="btn btn-success" type="submit"><i class="fa fa-fw fa-regular fa-eye"></i></button>
-              </form>
-            </td>
-            <td>
-              <form action="{{route('entrenadores.destroy',encrypt($entrenador->id))}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger" onclick="return eliminarEntrenador('Eliminar entrenador')"><i class="fa fa-fw fa-regular fa-trash"></i></button>
-              </form>
-            </td>
-          </tr>
-          @php
-            $contador++;
-          @endphp
-          @endforeach
+          @if(count($entrenadores)<=0)
+            <tr>
+              <td colspan="9" class="textoCentrado textoNegrita">NO SE ENCONTRARON RESULTADOS</td>
+            </tr>
+          @else
+            @foreach ($entrenadores as $entrenador)
+              <tr>
+                <td>{{$contador}}</td>
+                <td>{{$entrenador->nombre1}} {{$entrenador->nombre2}} {{$entrenador->nombre3}} {{$entrenador->apellido1}} {{$entrenador->apellido2}} {{$entrenador->apellido_casada}}</td>
+                <td>{{$entrenador->cui}}</td>
+                <td>{{$entrenador->estado_civil}}</td>
+                <td>{{$entrenador->celular}}</td>
+                <td>{{$entrenador->correo}}</td>
+                <td>{{$entrenador->edad}}</td>
+                <td>{{$entrenador->direccion}}</td>
+                <td>{{$entrenador->deporte->nombre}}</td>
+                <td>
+                  <form action="{{route('entrenadores.edit',encrypt($entrenador->id))}}" method="GET">
+                    <button class="btn btn-primary" type="submit"><i class="fa fa-fw fa-regular fa-edit"></i></button>
+                  </form>
+                </td>
+                <td>
+                  <form action="{{route('entrenadores.show',encrypt($entrenador->id))}}" action="GET">
+                    <button class="btn btn-success" type="submit"><i class="fa fa-fw fa-regular fa-eye"></i></button>
+                  </form>
+                </td>
+                <td>
+                  <form action="{{route('entrenadores.destroy',encrypt($entrenador->id))}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger" onclick="return eliminarEntrenador('Eliminar entrenador')"><i class="fa fa-fw fa-regular fa-trash"></i></button>
+                  </form>
+                </td>
+              </tr>
+              @php
+                $contador++;
+              @endphp
+            @endforeach
+          @endif
         </tbody>
       </table>
     </div>
@@ -98,4 +104,7 @@
 @push('js')
 <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
 <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
+@push('styles')
+  <link rel="stylesheet" href="css/general.css">
 @endpush
